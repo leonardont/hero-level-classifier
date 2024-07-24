140c75dacc84a48e7046eeb08fd604d4407144f4
Leonardo Nabarro Tonezer <leonardont2002@gmail.com>
2024-07-24 15:00:30 -0300

    fix: minor design changes.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function getLevelNameByXP(heroXPLevel) {
   const levels = [
-    { max: 1000, name: "Iron" },
-    { max: 2000, name: "Bronze" },
-    { max: 3000, name: "Silver" },
-    { max: 5000, name: "Gold" },
-    { max: 7000, name: "Platinum" },
-    { max: 8000, name: "Diamond" },
-    { max: 9000, name: "Ascendant" },
-    { max: 10000, name: "Immortal" },
+    { max: 10000, name: "Iron" },
+    { max: 20000, name: "Bronze" },
+    { max: 30000, name: "Silver" },
+    { max: 50000, name: "Gold" },
+    { max: 70000, name: "Platinum" },
+    { max: 80000, name: "Diamond" },
+    { max: 90000, name: "Ascendant" },
+    { max: 100000, name: "Immortal" },
     { max: Infinity, name: "Radiant" },
   ];
 
@@ -22,7 +22,11 @@ function changeHeroDescription() {
   const heroXPLevelFormatted = parseInt(heroXPLevel).toLocaleString();
   const content = document.getElementById("heroDescription");
 
-  content.innerHTML = `Your challenger, ${heroName}, is currently in rank ${getLevelNameByXP(
-    heroXPLevel
-  )} with ${heroXPLevelFormatted} points!`;
+  content.innerHTML = `Your hero, 
+                      <span class="font-bold font-mono">${heroName}</span>, 
+                      currently has 
+                      <span class="text-blue-400 font-bold font-mono">${heroXPLevelFormatted}</span>
+                      XP points and is placed at rank 
+                      <span class="border border-yellow-300 rounded-2xl bg-zinc-700 text-amber-300 font-bold font-mono pb-1 px-3 me-1">
+                      ${getLevelNameByXP(heroXPLevel)}</span>!`;
 }
